811faa9acce5e2e20244e5ef480991b4588c9ad0
Dave van der Goot <dave@salonkee.lu>
2020-02-21 17:48:59 +0100

    modified Css + layout | start button changes text to corresponding status

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,7 +1,10 @@
-const startBtn = document.getElementById("mainBtn");
-const resetBtn = document.getElementById("resetBtn");
-const timer = document.getElementById("timer");
-const interval = 1000;
+const startBtn  = document.getElementById("mainBtn");
+const resetBtn  = document.getElementById("resetBtn");
+const timer     = document.getElementById("timer");
+const breakTime = document.getElementById('breakTime').value || 5;
+
+const interval   = 1000;
+
 let time = 25;
 let duration = moment.duration(time, "minutes");
 let timerOn = false;
@@ -10,11 +13,14 @@ let intervalId;
 const countDown = function(){
     if(timerOn === false && !intervalId){
         timerOn = true;
+        startBtn.textContent = "Pause";
+
         intervalId = (setInterval(function(){
-        duration.subtract(interval, "milliseconds");
-        timer.textContent = moment(duration.asMilliseconds()).format('mm:ss')
+            duration.subtract(interval, "milliseconds");
+            timer.textContent = moment(duration.asMilliseconds()).format('mm:ss')
         }, interval));
     } else {
+        startBtn.textContent = "Continue";
         timerOn = false;
         clearInterval(intervalId);
         intervalId = null;
